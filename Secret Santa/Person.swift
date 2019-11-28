38e1359a6b3b40da8cba3e5b9e6170b68b828b89
Bizmarky <memccallum98@gmail.com>
2019-11-27 21:47:38 -0500

    Added person class

---
+++ Secret Santa/Person.swift
@@ -0,0 +1,41 @@
+//
+//  Person.swift
+//  Secret Santa
+//
+//  Created by Marcus McCallum on 11/27/19.
+//  Copyright © 2019 bizmarky. All rights reserved.
+//
+
+import Foundation
+import SwiftUI
+
+class Person: Any {
+    
+    var name: String = ""
+    var wishlist = [String]()
+    
+    init(name: String) {
+        self.name = name
+    }
+    
+    func getName() -> String {
+        return self.name
+    }
+    
+    func getWishlist() -> [String] {
+        return self.wishlist
+    }
+    
+    func setName(newName: String) {
+        self.name = newName
+    }
+    
+    func addToWishlist(item: String) {
+        self.wishlist.append(item)
+    }
+    
+    func removeFromWishlist(index: Int) {
+        self.wishlist.remove(at: index)
+    }
+    
+}
