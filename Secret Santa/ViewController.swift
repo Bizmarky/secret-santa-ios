63bed7555e847b7a5ab1636bc9f7f339eaba177d
Augustine Ubah <augustine_ubah@yahoo.com>
2019-11-27 22:34:12 -0500

    hard code name

--- Secret Santa/ViewController.swift
+++ Secret Santa/ViewController.swift
@@ -10,11 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var userName: UITextView!
+    @IBOutlet weak var userWishList: UITextView!
+    @IBOutlet weak var submitButtom: UIButton!
+    
+    var user = Person
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
 
+    let p1 = Person(name:"Au")
+    let p2 = Person(name:"Ma")
+    let p3 = Person(name:"Gu")
+    let p4 = Person(name:"Su")
+    let p5 = Person(name:"Ta")
+    
 
 }
 
