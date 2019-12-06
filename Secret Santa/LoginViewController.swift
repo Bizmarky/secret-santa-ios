9ac25f314c62a6b16ba23255f1a34a55fac3eed2
Bizmarky <memccallum98@gmail.com>
2019-12-06 11:37:17 -0500

    Added forgot password

--- Secret Santa/LoginViewController.swift
+++ Secret Santa/LoginViewController.swift
@@ -20,6 +20,14 @@ class LoginViewController: UIViewController, UITextFieldDelegate {
         
     @IBOutlet weak var activityIndicatorView: UIActivityIndicatorView!
     
+    var pwAlert: UIAlertController!
+    
+    @IBAction func passwordAction(_ sender: Any) {
+        
+        self.present(pwAlert, animated: true, completion: nil)
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -38,7 +46,29 @@ class LoginViewController: UIViewController, UITextFieldDelegate {
         emailTextField.becomeFirstResponder()
         
         activityIndicatorView.isHidden = true
-                
+        self.setupToHideKeyboardOnTapOnView()
+        pwAlert = UIAlertController(title: "Reset Password", message: "Please enter your email below", preferredStyle: .alert)
+        pwAlert.addTextField()
+        pwAlert.textFields![0].keyboardType = .emailAddress
+        pwAlert.textFields![0].placeholder = "Email"
+        pwAlert.textFields![0].clearButtonMode = .whileEditing
+        pwAlert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        pwAlert.addAction(UIAlertAction(title: "Reset Password", style: .default, handler: { (action) in
+            let field = self.pwAlert.textFields![0]
+            if field.text! != "" {
+                Auth.auth().sendPasswordReset(withEmail: field.text!) { (err) in
+                    if let err = err {
+                        createAlert(view: self, title: "Error", message: err.localizedDescription)
+                    } else {
+                        createAlert(view: self, title: "Success", message: "Password reset email has been sent to "+field.text!)
+                    }
+                }
+            } else {
+                createAlert(view: self, title: "Error", message: "Text field cannot be blank") { (complete) in
+                    self.present(self.pwAlert, animated: true, completion: nil)
+                }
+            }
+        }))
     }
     
     @objc func deletedAccount() {
